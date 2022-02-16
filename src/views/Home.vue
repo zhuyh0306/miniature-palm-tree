365f57d2bf5256cbeb0d385a300b1105e905b912
zhuyh <zhuyhd@cn.ibm.com>
2022-02-16 14:04:38 +0800

    feat(项目初始化): 新增项目配置

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <HelloWorld1 msg="Welcome to Your Vue.js App" />
+    <HelloWorld2 msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld1 from '@/components/HelloWorld.vue'
+import HelloWorld2 from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld,
+    HelloWorld1,
+    HelloWorld2
+  }
+}
+</script>
